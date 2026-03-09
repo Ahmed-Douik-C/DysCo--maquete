6abf6c340746195f6cbc7e0c4431553c549bebbf
Kais Triki <cryopt097@gmail.com>
2026-03-09 10:26:56 +0100

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-var AdminMode = false;
+var AdminMode = sessionStorage.getItem('AdminMode') === 'true';
+
+function setAdminMode(value) {
+    AdminMode = value;
+    sessionStorage.setItem('AdminMode', String(value));
+}
+
+document.querySelectorAll('.log-btn')[0].addEventListener('click', () => {
+    setAdminMode(true);
+
+});
+document.querySelectorAll('.log-btn')[1].addEventListener('click', () => {
+    setAdminMode(false);
+});
